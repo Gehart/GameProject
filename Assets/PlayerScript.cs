7bae9f5fe28d9dbb6ceae9d39e12b7f82d418fd4
Gleb <g.orlovi@ya.ru>
2020-04-09 11:41:11 +0300

    Добавление смерти игрока
    Add death

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject DeadPlayer;
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cactus")
+        {
+            Player.SetActive(false);
+            DeadPlayer.SetActive(true);
+            Instantiate(DeadPlayer, transform.position, transform.rotation);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
