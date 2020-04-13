699df337d406e93e25635c617e980ee6cf178386
Gleb <g.orlovi@ya.ru>
2020-04-13 13:26:13 +0300

    Анимация, подводные эффекты
    Анимация, подводные эффекты, незначительные изменения в террейне

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -17,11 +17,17 @@ public class PlayerScript : MonoBehaviour
             DeadPlayer.SetActive(true);
             Instantiate(DeadPlayer, transform.position, transform.rotation);
         }
-
+        if (other.tag == "DeadObject")
+        {
+            Debug.Log("dead!");
+            Player.GetComponent<Animator>().SetTrigger("Dead");
+        }
         if (other.tag == "Water")
         {
             // ApplyDamage(10);
-            Debug.Log("water!"); 
+            Debug.Log("water!");
+            RenderSettings.fogDensity = 0.2f;
+            RenderSettings.fogColor = new Color(0.6f, 0.64f, 0.99f, 1f);
             RenderSettings.fog = true;
         }
     }
