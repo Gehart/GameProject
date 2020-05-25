483dab7871d134546f0967b343db887ab89718ed
Gleb <g.orlovi@ya.ru>
2020-05-25 15:22:57 +0300

    Developing enemy's coordination

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;
+
+    void Start()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Instantiate(enemy, new Vector3(24.64f + i * 2f, 107.9567f, 45.58f), Quaternion.identity);
+
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
